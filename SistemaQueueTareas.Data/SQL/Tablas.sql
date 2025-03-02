e09759db4b8c243c2d106afa9383ac3531cdbfc4
Allan1418 <allannb02@gmail.com>
2025-03-01 19:27:44 -0600

    Add project files.

---
+++ SistemaQueueTareas.Data/SQL/Tablas.sql
@@ -0,0 +1,27 @@
+﻿
+
+------------------------------
+USE master
+GO
+
+IF EXISTS (SELECT name FROM sys.databases WHERE name = 'SistemaQueueTareas')
+BEGIN
+    ALTER DATABASE [SistemaQueueTareas] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+    DROP DATABASE [SistemaQueueTareas]
+END
+GO
+
+CREATE DATABASE [SistemaQueueTareas]
+GO
+
+USE [SistemaQueueTareas]
+GO
+------------------------------
+
+CREATE TABLE Users (
+    id INT PRIMARY KEY,
+    Nombre VARCHAR(50),
+    Apellido VARCHAR(50),
+    Email VARCHAR(100),
+    Telefono VARCHAR(20)
+);
