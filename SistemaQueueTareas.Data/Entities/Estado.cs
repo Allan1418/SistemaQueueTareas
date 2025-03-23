cbb800954b1c59fc9c39913e0bd6f8a12c8aac90
Elder <elderbarrantesp@gmail.com>
2025-03-23 15:47:13 -0600

    Creacion de las clases parciales

---
+++ SistemaQueueTareas.Data/Entities/Estado.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaQueueTareas.Data
+{
+    public partial class Estado : Entity
+    {
+    }
+}
